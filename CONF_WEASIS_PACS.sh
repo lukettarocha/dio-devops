8fc908dd2fd68fc948b41df2d760e39c5f89d0ec
lukettarocha <lucasleiterocha97@gmail.com>
2022-11-17 16:35:44 -0300

    Update CONF_WEASIS_PACS.sh

--- CONF_WEASIS_PACS.sh
+++ CONF_WEASIS_PACS.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#BAIXANDO OS ARQUIVOS DO WEASUS
+#DECLARANDO AS VARIÁVEIS DO AE-TITLE E DA PORTA PADRÃO
+read -p "Digite o AE-TITLE do seu PACS: " VAR_AETITLE
+read -p "Digite a PORTA PADRÃO do seu PACS: " VAR_PORTA
 
+#BAIXANDO OS ARQUIVOS DO WEASUS
 wget --no-check-certificate https://sourceforge.net/projects/dcm4che/files/Weasis/weasis-pacs-connector/6.1.5/dcm4chee-web-weasis.jar/download -O /opt/pacs/dcm4chee/server/default/deploy/dcm4chee-web-weasis.jar && wget --no-check-certificate https://sourceforge.net/projects/dcm4che/files/Weasis/weasis-pacs-connector/6.1.5/weasis-pacs-connector.war/download -O /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war && wget --no-check-certificate https://sourceforge.net/projects/dcm4che/files/Weasis/3.8.1/weasis.war/download -O /opt/pacs/dcm4chee/server/default/deploy/weasis.war
 
 sleep 5
@@ -17,31 +20,15 @@ wget --no-check-certificate https://raw.githubusercontent.com/nroduit/weasis-pac
 sleep 5
 
 #EDITANDO OS ARQUIVOS DE CONFIGURAÇÃO NA PASTA DO WEASIS CONNECTOR
-
 #PARTE 1: CRIANDO A PASTA PARA EXTRAIR O ARQUIVO.WAR
-cd /opt/pacs/dcm4chee/server/default/deploy/
-chmod 777 weasis-pacs-connector.war
-mv weasis-pacs-connector.war weasis-pacs-connector.war.legacy
-mkdir weasis-pacs-connector.war
-chmod 777 weasis-pacs-connector.war
-cd weasis-pacs-connector.war
-mv weasis-pacs-connector.war.legacy weasis-pacs-connector.war
-jar -xvf weasis-pacs-connector.war
-rm -rf weasis-pacs-connector.war
+chmod 777 /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war
+mv /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war.legacy
+mkdir /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war
+chmod 777 /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war
+mv /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war.legacy /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/weasis-pacs-connector.war
+unzip /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/weasis-pacs-connector.war -d /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/
+#rm -rf /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/weasis-pacs-connector.war
 
 #PARTE 2: EDITANDO O ARQUIVO DE CONFIGURAÇÃO
-
-sed -i '11s/DCM4CHEE/$VAR_AETITLE/' /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/WEB-INF/classes/dicom-dcm4chee.properties
-sed -i '13s/11112/$VAR_PORTA/' /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/WEB-INF/classes/dicom-dcm4chee.properties
-
-
-
-
-
-
-
-
-#EDITAR ARQUIVO DE CONFIGURAÇÃO DO dicom-dcm4chee.properties JBOSS CONF
-#vim /opt/pacs/jboss/server/default/conf/dicom-dcm4chee-properties
-#ALTERAR O AE-TITLE E A PORTA DE COMUNICAÇÃO DO LOCAL SERVER
-s
+sed -i '11s/DCM4CHEE/'${VAR_AETITLE}'/' /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/WEB-INF/classes/dicom-dcm4chee.properties
+sed -i '13s/11112/'${VAR_PORTA}'/' /opt/pacs/dcm4chee/server/default/deploy/weasis-pacs-connector.war/WEB-INF/classes/dicom-dcm4chee.properties
